9d61cb05944f7f8039b83a884f5f235588ca4849
Fan Long <cctv1237@live.com>
2018-07-24 17:15:39 +0800

    Add states list of MY

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const postcode = require('./postcode');
 module.exports = {
     postcode: postcode.postcode,
     search: postcode.search,
+    states: postcode.states,
     statePostcodes: postcode.statePostcodes,
     cityPostcodes: postcode.cityPostcodes,
     districtPostcodes: postcode.districtPostcodes,
